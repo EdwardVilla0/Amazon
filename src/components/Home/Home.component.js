2e50cbbcaeedd48d8101c6e326fa17c2b657ca45
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-01-12 12:20:56 -0800

    fixing background image

--- src/components/Home/Home.component.js
+++ src/components/Home/Home.component.js
@@ -7,8 +7,7 @@ function Home() {
             <img
                 className="home__image"
                 alt="amazon background"
-                src="https://store-images.s-microsoft.com/image/apps.16285.14618985536919905.552c0017-6644-49a8-8467-8f7b34ce0428.30ad6b05-16d9-4d5e-a242-43107708a16a?mode=scale&q=90&h=1080&w=1920" />
-
+                src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" />
         </div>
     )
 }
