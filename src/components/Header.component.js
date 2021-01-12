e777909867b73a2f104717abb906ac94e01af238
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-01-11 17:05:57 -0800

    installed material UI

--- src/components/Header.component.js
+++ src/components/Header.component.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.styles.css';
+import SearchIcon from '@material-ui/icons/Search';
+
 
 function Header() {
     return (
         <nav className="header">
-            <img
-                className="header__logo"
-                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
-                alt="" />
+            <Link to="/">
+                <img
+                    className="header__logo"
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                    alt="" />
+            </Link>
+
+            <input type="text" className="header__searchInput" />
+            <SearchIcon className="header__searchIcon" />
         </nav>
     )
 }
