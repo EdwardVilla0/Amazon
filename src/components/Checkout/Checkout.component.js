5c0f36648d86a1379a3e6b7409c114c897a52ef0
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-01-14 12:56:31 -0800

    created message for the checkout component

--- src/components/Checkout/Checkout.component.js
+++ src/components/Checkout/Checkout.component.js
@@ -6,13 +6,21 @@ function Checkout() {
     const [{ basket }] = useStateValue();
     return (
         <div className="checkout">
-            <div className="">
-                <img
-                    className="chechout__ad"
-                    src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/Movie/ONIM_2021_GWBleedingHero_1500x600_PRE_Final_en-US_PVD6356_OneNightInMiami._CB411496424_.jpg"
-                    alt="" />
+            <img
+                className="chechout__ad"
+                src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/Movie/ONIM_2021_GWBleedingHero_1500x600_PRE_Final_en-US_PVD6356_OneNightInMiami._CB411496424_.jpg"
+                alt="" />
 
-            </div>
+            {basket?.length === 0 ? (
+                <div>
+                    <h2>your shopping cart is empty</h2>
+                </div>
+            ) : (
+                    <div>
+                        <h1>testing more</h1>
+                    </div>
+                )
+            }
         </div>
     )
 }
