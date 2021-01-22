045f5617a73d2966cb7297400001d612a8d0c848
Eduardo Villasenor <eduardovillasenor1992@gmail.com>
2021-01-21 18:05:05 -0800

    using firebase auth

--- src/components/Login/Login.component.js
+++ src/components/Login/Login.component.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.styles.css';
 import { Link, useHistory } from 'react-router-dom';
+import { auth } from '../../firebase/firebase';
 
-const login = event => {
-    event.preventDefault();
-}
-
-const register = event => {
-    event.preventDefault();
-}
 
 function Login() {
+    const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const login = event => {
+        event.preventDefault();
+
+        auth.signInWithEmailAndPassword(email, password)
+            .then((auth) => {
+                history.push('/');
+            })
+            .catch(e => alert(e.message));
+    }
+
+    const register = event => {
+        event.preventDefault();
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .then(auth => {
+
+            })
+            .catch((e) => alert(e.message));
+    }
+
+
     return (
         <div className="login">
             <Link to="/">
@@ -24,9 +43,9 @@ function Login() {
                 <h1>Sign in</h1>
                 <form>
                     <h5>Email</h5>
-                    <input type="email" />
+                    <input value={email} onChange={event => setEmail(event.target.value)} type="email" />
                     <h5>Password</h5>
-                    <input type="password" />
+                    <input value={password} onChange={event => setPassword(event.target.value)} type="password" />
                     <button onClick={login} type="submit" className="login__containerSignInButton">Sign In</button>
                 </form>
 
